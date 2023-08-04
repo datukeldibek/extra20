299e8e2d97623b3026350f372d30740ca2c57d9b
datukeldibek <118820830+datukeldibek@users.noreply.github.com>
2023-08-04 15:48:28 +0600

    Initial Commit

---
+++ extra20/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  extra20
+//
+//  Created by Jarae on 4/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
